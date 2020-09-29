804fc2e18c0b97f6fe193216e09b538b52598c95
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-09-29 18:40:48 +0530

    intial Setup

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./App.scss";
+
+
+function App() {
+  return (
+
+    <div className="app-container">
+        <p>My-Profile</p>
+        <p>User-Visiting-Profile</p>
+        <p>User-Visiting-Photos and Stories</p>
+    </div>
+
+  );
+}
+
+export default App;
